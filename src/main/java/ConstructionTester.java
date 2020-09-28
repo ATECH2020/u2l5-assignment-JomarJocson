0f544f75574c2595b18836466b5d38ad0f2a2c90
Jomar Jocson <jomar.224585@nv.ccsd.net>
2020-09-28 01:29:08 +0000

    Tester

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,21 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+      double taxRate;
+      int numBoards;
+      int numWindows;
+
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Enter the sales tax rate:");
+      taxRate = scanner.nextDouble();
+
+      System.out.println("How many boards do you need?");
+      numBoards = scanner.nextInt();
+
+      System.out.println("How many windows do you need?");
+      numWindows = scanner.nextInt();
+
+    Construction myConstruction = new Construction(8.0,11.0,taxRate);
     }
 }
